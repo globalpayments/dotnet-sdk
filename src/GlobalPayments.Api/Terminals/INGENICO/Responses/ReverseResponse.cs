51ce2c8d7a1dc3c0740a9ab020797af93e97290c
peter.yanong.serino <peter.yanong@serino.com>
2020-03-03 23:14:08 +0800

    Handle Reverse response
    Create reverse response and inherits IgenicoTerminalResponse.
    Override ParseResponse so that status will change to Reverse specific status.

---
+++ src/GlobalPayments.Api/Terminals/INGENICO/Responses/ReverseResponse.cs
@@ -0,0 +1,18 @@
+﻿using GlobalPayments.Api.Terminals.Abstractions;
+using GlobalPayments.Api.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlobalPayments.Api.Terminals.INGENICO {
+    public class ReverseResponse : IngenicoTerminalResponse, IDeviceResponse{
+        public ReverseResponse(byte[] buffer) : base(buffer) {
+            ParseResponse(buffer);
+        }
+
+        public override void ParseResponse(byte[] response) {
+            base.ParseResponse(response);
+            Status = ((ReverseStatus)Encoding.UTF8.GetString(response.SubArray(2, 1)).ToInt32()).ToString();
+        }
+    }
+}
