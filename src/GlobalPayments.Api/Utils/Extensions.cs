23ba14bce9aaaf4b3adb074ac6fc02f1f92dc5ff
peter.yanong.serino <peter.yanong@serino.com>
2020-03-03 23:08:20 +0800

    Added some methods
    Added SubArray method, to get sub array data.
    Added IsNull method for checking of null value.

--- src/GlobalPayments.Api/Utils/Extensions.cs
+++ src/GlobalPayments.Api/Utils/Extensions.cs
@@ -206,5 +206,14 @@ namespace GlobalPayments.Api.Utils {
             }
             return rvalue;
         }
+        public static T[] SubArray<T>(this T[] data, int index, int length) {
+            T[] result = new T[length];
+            Array.Copy(data, index, result, 0, length);
+            return result;
+        }
+
+        public static bool IsNull<T>(this T data) {
+            return data == null;
+        }
     }
 }
