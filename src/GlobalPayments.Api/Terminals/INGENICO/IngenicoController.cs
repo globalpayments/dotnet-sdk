cef3293215121bb8848f243b33bb4985581d2710
peter-yanong <peteryanongwork@gmail.com>
2020-03-28 18:09:30 +0800

    Ingenico Controller
    Add brackets to single line if statements.
    Change var to exact data type for ambiguous data.
    Use StringBuilder class for building mass string data instead of string format method

--- src/GlobalPayments.Api/Terminals/INGENICO/IngenicoController.cs
+++ src/GlobalPayments.Api/Terminals/INGENICO/IngenicoController.cs
@@ -1,11 +1,8 @@
 ﻿using GlobalPayments.Api.Entities;
-using GlobalPayments.Api.PaymentMethods;
 using GlobalPayments.Api.Terminals.Abstractions;
 using GlobalPayments.Api.Terminals.Builders;
-using GlobalPayments.Api.Terminals.Messaging;
 using GlobalPayments.Api.Utils;
 using System;
-using System.Collections.Generic;
 using System.Text;
 
 namespace GlobalPayments.Api.Terminals.INGENICO {
@@ -21,6 +18,7 @@ namespace GlobalPayments.Api.Terminals.INGENICO {
             }
             return _device;
         }
+
         internal override IDeviceCommInterface ConfigureConnector() {
             switch (_settings.ConnectionMode) {
                 case ConnectionModes.SERIAL:
@@ -37,7 +35,7 @@ namespace GlobalPayments.Api.Terminals.INGENICO {
 
         #region overrides
         internal override ITerminalResponse ManageTransaction(TerminalManageBuilder builder) {
-            var request = BuildManageTransaction(builder);
+            IDeviceMessage request = BuildManageTransaction(builder);
 
             if (builder.TransactionType == TransactionType.Cancel) {
                 return DoCancelRequest(request);
@@ -51,21 +49,21 @@ namespace GlobalPayments.Api.Terminals.INGENICO {
         }
 
         internal override ITerminalReport ProcessReport(TerminalReportBuilder builder) {
-            var request = TerminalUtilities.BuildIngenicoRequest(INGENICO_REQ_CMD.RECEIPT.FormatWith(builder.ReceiptType), _settings.ConnectionMode);
+            IDeviceMessage request = TerminalUtilities.BuildIngenicoRequest(INGENICO_REQ_CMD.RECEIPT.FormatWith(builder.ReceiptType), _settings.ConnectionMode);
             return ReportRequest(request);
         }
 
         internal override ITerminalResponse ProcessTransaction(TerminalAuthBuilder builder) {
-            var request = BuildRequestMessage(builder);
+            IDeviceMessage request = BuildProcessTransaction(builder);
             return DoRequest(request);
         }
 
         internal override byte[] SerializeRequest(TerminalAuthBuilder builder) {
-            return BuildRequestMessage(builder).GetSendBuffer();
+            return BuildProcessTransaction(builder).GetSendBuffer();
         }
 
         internal override byte[] SerializeRequest(TerminalManageBuilder builder) {
-            throw new NotImplementedException();
+            return BuildManageTransaction(builder).GetSendBuffer();
         }
 
         internal override byte[] SerializeRequest(TerminalReportBuilder builder) {
@@ -75,121 +73,135 @@ namespace GlobalPayments.Api.Terminals.INGENICO {
 
         #region Methods
         internal IDeviceMessage BuildManageTransaction(TerminalManageBuilder builder) {
-            var _refNumber = builder.ReferenceNumber;
-            var _amount = ValidateAmount(builder.Amount);
-            var _returnRep = 1;
-            var _paymentMode = ValidatePaymentMode(builder.PaymentMode);
-            var _paymentType = (int?)((IngenicoInterface)_device).paymentMethod ?? 0;
-            var _currencyCode = (string.IsNullOrEmpty(builder.CurrencyCode) ? "826" : builder.CurrencyCode);
-            var _privData = "EXT0100000";
-            var _immediateAns = 0;
-            var _forceOnline = 0;
-            var _extendedData = "0000000000";
+            int refNumber = builder.ReferenceNumber;
+            decimal? amount = ValidateAmount(builder.Amount);
+            int returnRep = 1;
+            int paymentMode = ValidatePaymentMode(builder.PaymentMode);
+            int? paymentType = (int?)((IngenicoInterface)_device).paymentMethod ?? 0;
+            string currencyCode = (string.IsNullOrEmpty(builder.CurrencyCode) ? "826" : builder.CurrencyCode);
+            string privData = "EXT0100000";
+            int immediateAns = 0;
+            int forceOnline = 0;
+            string extendedData = "0000000000";
 
             // For Auth code value in extended data
             if (!string.IsNullOrEmpty(builder.AuthCode))
-                _extendedData = ValidateExtendedData(builder.AuthCode, builder.ExtendedDataTags);
+                extendedData = ValidateExtendedData(builder.AuthCode, builder.ExtendedDataTags);
             else if (!builder.TransactionId.IsNull() && builder.TransactionType == TransactionType.Reversal)
                 // For Reversal with Transaction Id value in extended data
-                _extendedData = ValidateExtendedData(builder.TransactionId, ExtendedDataTags.TXN_COMMANDS_PARAMS);
+                extendedData = ValidateExtendedData(builder.TransactionId, ExtendedDataTags.TXN_COMMANDS_PARAMS);
             else
-                _extendedData = ValidateExtendedData(builder.TransactionType.ToString(), ExtendedDataTags.TXN_COMMANDS);
+                extendedData = ValidateExtendedData(builder.TransactionType.ToString(), ExtendedDataTags.TXN_COMMANDS);
 
-            string message = string
-                .Format("{0}{1}{2}{3}{4}{5}{6}A01{7}B01{8}{9}",
-                builder.ReferenceNumber.ToString("00"),
-                _amount?.ToString("00000000"),
-                _returnRep,
-                _paymentMode,
-                _paymentType,
-                _currencyCode,
-                _privData,
-                _immediateAns,
-                _forceOnline,
-                _extendedData
-                );
+            // Concat all data to create a request string.
+            var sb = new StringBuilder();
 
-            return TerminalUtilities.BuildIngenicoRequest(message, _settings.ConnectionMode);
+            sb.Append(builder.ReferenceNumber.ToString("00"));
+            sb.Append(amount?.ToString("00000000"));
+            sb.Append(returnRep);
+            sb.Append(paymentMode);
+            sb.Append(paymentType);
+            sb.Append(currencyCode);
+            sb.Append(privData);
+            sb.Append("A01" + immediateAns);
+            sb.Append("B01" + forceOnline);
+            sb.Append(extendedData);
+
+            return TerminalUtilities.BuildIngenicoRequest(sb.ToString(), _settings.ConnectionMode);
         }
 
-        internal IDeviceMessage BuildRequestMessage(TerminalAuthBuilder builder) {
+        internal IDeviceMessage BuildProcessTransaction(TerminalAuthBuilder builder) {
             string message = string.Empty;
             if (!IsObjectNullOrEmpty(builder.ReportType))
                 message = INGENICO_REQ_CMD.REPORT.FormatWith(builder.ReportType);
             else {
-                var _referenceNumber = builder.ReferenceNumber;
-                var _amount = builder.Amount;
-                var _returnRep = 1;
-                var _paymentMode = 0;
-                var _paymentType = (int)((IngenicoInterface)_device).paymentMethod;
-                var _currencyCode = "826";
-                var _privateData = "EXT0100000";
-                var _immediateAnswer = 0;
-                var _forceOnline = 0;
-                var _extendedData = "0000000000";
+                int referenceNumber = builder.ReferenceNumber;
+                decimal? amount = builder.Amount;
+                int returnRep = 1;
+                int paymentMode = 0;
+                int paymentType = (int)((IngenicoInterface)_device).paymentMethod;
+                string currencyCode = "826";
+                string privateData = "EXT0100000";
+                int immediateAnswer = 0;
+                int forceOnline = 0;
+                string extendedData = "0000000000";
 
-                var _cashbackAmount = builder.CashBackAmount;
-                var _authCode = builder.AuthCode;
+                decimal? cashbackAmount = builder.CashBackAmount;
+                string authCode = builder.AuthCode;
                 string tableId = builder.TableNumber;
 
                 // Validations
-                _amount = ValidateAmount(_amount);
-                _paymentMode = ValidatePaymentMode(builder.PaymentMode);
-                _currencyCode = ValidateCurrency((string.IsNullOrEmpty(builder.CurrencyCode) ? _currencyCode : builder.CurrencyCode));
+                amount = ValidateAmount(amount);
+                paymentMode = ValidatePaymentMode(builder.PaymentMode);
+                currencyCode = ValidateCurrency((string.IsNullOrEmpty(builder.CurrencyCode) ? currencyCode : builder.CurrencyCode));
 
                 if (!string.IsNullOrEmpty(tableId)) {
                     bool validateTableId = ValidateTableReference(tableId);
                     if (validateTableId)
-                        _extendedData = ValidateExtendedData(tableId, builder.ExtendedDataTags);
+                        extendedData = ValidateExtendedData(tableId, builder.ExtendedDataTags);
                 }
 
-                if (!IsObjectNullOrEmpty(_cashbackAmount))
-                    _extendedData = ValidateExtendedData(_cashbackAmount.ToString(), builder.ExtendedDataTags);
-                else if (!string.IsNullOrEmpty(_authCode))
-                    _extendedData = ValidateExtendedData(_authCode, builder.ExtendedDataTags);
+                if (!IsObjectNullOrEmpty(cashbackAmount))
+                    extendedData = ValidateExtendedData(cashbackAmount.ToString(), builder.ExtendedDataTags);
+                else if (!string.IsNullOrEmpty(authCode))
+                    extendedData = ValidateExtendedData(authCode, builder.ExtendedDataTags);
 
+                // Concat all data to create a request string.
+                var sb = new StringBuilder();
 
+                sb.Append(referenceNumber.ToString("00"));
+                sb.Append(amount?.ToString("00000000"));
+                sb.Append(returnRep);
+                sb.Append(paymentMode);
+                sb.Append(paymentType);
+                sb.Append(currencyCode);
+                sb.Append(privateData);
+                sb.Append("A01" + immediateAnswer);
+                sb.Append("B01" + forceOnline);
+                sb.Append(extendedData);
 
-                message = string.Format("{0}{1}{2}{3}{4}{5}{6}A01{7}B01{8}{9}",
-                    _referenceNumber.ToString("00"),
-                    _amount?.ToString("00000000"),
-                    _returnRep,
-                    _paymentMode,
-                    _paymentType,
-                    _currencyCode,
-                    _privateData,
-                    _immediateAnswer,
-                    _forceOnline,
-                    _extendedData);
+                message = sb.ToString();
             }
 
             return TerminalUtilities.BuildIngenicoRequest(message, _settings.ConnectionMode);
+        }
+
+        internal IngenicoTerminalReportResponse ReportRequest(IDeviceMessage request) {
+            var send = Send(request);
+            return new IngenicoTerminalReportResponse(send);
         }
 
         #region Validations
         private static bool IsObjectNullOrEmpty(object value) {
             bool response = false;
-            if (value.IsNull() || string.IsNullOrWhiteSpace(value.ToString()))
+            if (value.IsNull() || string.IsNullOrWhiteSpace(value.ToString())) {
                 response = true;
-            else response = false;
-
-            return response;
-        }
-        private static bool ValidateTableReference(string value) {
-            bool response = false;
-            if (!string.IsNullOrEmpty(value) && value.Length <= 8)
-                response = true;
-            else throw new BuilderException("Table number must not be less than or equal 0 or greater than 8 numerics.");
-
-            return response;
-        }
-
-        private static int ValidatePaymentMode(PaymentMode _paymentMode) {
-            if (IsObjectNullOrEmpty(_paymentMode)) {
-                _paymentMode = PaymentMode.APPLICATION;
+            }
+            else {
+                response = false;
             }
 
-            return (int)_paymentMode;
+            return response;
+        }
+
+        private static bool ValidateTableReference(string value) {
+            bool response = false;
+            if (!string.IsNullOrEmpty(value) && value.Length <= 8) {
+                response = true;
+            }
+            else {
+                throw new BuilderException("Table number must not be less than or equal 0 or greater than 8 numerics.");
+            }
+
+            return response;
+        }
+
+        private static int ValidatePaymentMode(PaymentMode? paymentMode) {
+            if (IsObjectNullOrEmpty(paymentMode)) {
+                paymentMode = PaymentMode.APPLICATION;
+            }
+
+            return (int)paymentMode;
         }
 
         private static string ValidateExtendedData(string value, ExtendedDataTags tags) {
@@ -197,11 +209,15 @@ namespace GlobalPayments.Api.Terminals.INGENICO {
             switch (tags) {
                 case ExtendedDataTags.CASHB:
                     decimal? cashbackAmount = Convert.ToDecimal(value);
-                    if (cashbackAmount > 0m && cashbackAmount < 1000000m)
+                    if (cashbackAmount > 0m && cashbackAmount < 1000000m) {
                         cashbackAmount *= 100;
-                    else if (cashbackAmount <= 0m)
+                    }
+                    else if (cashbackAmount <= 0m) {
                         throw new BuilderException("Cashback Amount must not be in less than or equal 0 value.");
-                    else throw new BuilderException("Cashback Amount exceed.");
+                    }
+                    else {
+                        throw new BuilderException("Cashback Amount exceed.");
+                    }
 
                     extendedData = "CASHB={0};".FormatWith(Convert.ToInt64(Math.Round(cashbackAmount.Value, MidpointRounding.AwayFromZero)));
                     break;
@@ -233,54 +249,50 @@ namespace GlobalPayments.Api.Terminals.INGENICO {
             return extendedData;
         }
 
-        private static string ValidateCurrency(string _currencyCode) {
-            if (!string.IsNullOrWhiteSpace(_currencyCode)) {
-                _currencyCode = _currencyCode.PadLeft(3, '0');
+        private static string ValidateCurrency(string currencyCode) {
+            if (!string.IsNullOrWhiteSpace(currencyCode)) {
+                currencyCode = currencyCode.PadLeft(3, '0');
             }
-            else _currencyCode = "826";
+            else {
+                currencyCode = "826";
+            }
 
-            return _currencyCode;
+            return currencyCode;
         }
 
-        private decimal? ValidateAmount(decimal? _amount) {
-            if (_amount == null)
+        private decimal? ValidateAmount(decimal? amount) {
+            if (amount == null) {
                 throw new BuilderException("Amount can not be null.");
-            else if (_amount > 0 && _amount < 1000000m)
-                _amount *= 100;
-            else if (_amount >= 1000000m)
+            }
+            else if (amount > 0 && amount < 1000000m) {
+                amount *= 100;
+            }
+            else if (amount >= 1000000m) {
                 throw new BuilderException("Amount exceed.");
-            else
+            }
+            else {
                 throw new BuilderException("Invalid input amount.");
-            return _amount;
+            }
+            return amount;
         }
 
         #endregion
 
-        public ITerminalConfiguration GetConfiguration() {
-            return _settings;
-        }
-        #endregion
-
-        #region Report Request
-        internal IngenicoTerminalReportResponse ReportRequest(IDeviceMessage request) {
-            var send = Send(request);
-            return new IngenicoTerminalReportResponse(send);
-        }
         #endregion
 
         #region Request
         internal IngenicoTerminalResponse DoRequest(IDeviceMessage request) {
-            var response = Send(request);
+            byte[] response = Send(request);
             return new IngenicoTerminalResponse(response);
         }
 
         private CancelResponse DoCancelRequest(IDeviceMessage request) {
-            var response = Send(request);
+            byte[] response = Send(request);
             return new CancelResponse(response);
         }
 
         private ReverseResponse DoReverseRequest(IDeviceMessage request) {
-            var response = Send(request);
+            byte[] response = Send(request);
             return new ReverseResponse(response);
         }
         #endregion
