cbcb2b133d3b819ff1bae855e90be12ae7715894
Peter Yanong <peter.yanong@serino.com>
2020-04-03 19:10:19 +0800

    Create ReportResponse file
    Hande response from terminal when request is about report

---
+++ src/GlobalPayments.Api/Terminals/INGENICO/Responses/ReportResponse.cs
@@ -0,0 +1,16 @@
+﻿using GlobalPayments.Api.Terminals.Abstractions;
+using GlobalPayments.Api.Utils;
+using System.Text;
+
+namespace GlobalPayments.Api.Terminals.Ingenico.Responses {
+    public class ReportResponse : IngenicoTerminalResponse, ITerminalReport {
+        internal ReportResponse(byte[] buffer) : base(buffer) {
+            ParseResponse(buffer);
+        }
+
+        public override void ParseResponse(byte[] response) {
+            base.ParseResponse(response);
+            _privateData = Encoding.UTF8.GetString(response.SubArray(70, response.Length - 70));
+        }
+    }
+}
