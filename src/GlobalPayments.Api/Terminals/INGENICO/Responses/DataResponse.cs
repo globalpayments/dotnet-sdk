f0a7e6d6896a20039cb6b8f6169097acb9b66aca
Peter Yanong <peter.yanong@serino.com>
2020-05-08 19:07:09 +0800

    Refactor DataResponse file
    Remove private declaration of Response Code and move it to IngenicoEnums.
    Remove GetValueOfRespField method for parsing of response data.

--- src/GlobalPayments.Api/Terminals/INGENICO/Responses/DataResponse.cs
+++ src/GlobalPayments.Api/Terminals/INGENICO/Responses/DataResponse.cs
@@ -22,22 +22,6 @@ namespace GlobalPayments.Api.Terminals.Ingenico {
 
         private byte[] _buffer;
 
-        #region Byte Code
-        // For less memory allocation;
-        private byte _C = 67;
-        private byte _Z = 90;
-        private byte _Y = 89;
-        private byte _M = 77;
-        private byte _A = 65;
-        private byte _U = 85;
-        private byte _O = 79;
-        private byte _P = 80;
-        private byte _T = 84;
-        private byte _S = 83;
-        private byte _D = 68;
-        #endregion
-
-
         public DataResponse(byte[] buffer) {
             _buffer = buffer;
             ParseData();
@@ -101,67 +85,24 @@ namespace GlobalPayments.Api.Terminals.Ingenico {
 
         #endregion
 
-        /**
-         * C = AuthCode
-         * Z = Cashback Amount
-         * Y = Gratuity Amount
-         * M = Final Transaction Amount
-         * A = Available Amount
-         * U = DCC Currency
-         * O = DCC Converted transaction amount
-         * P = Payment Method
-         * T = Transaction Sub-Type
-         * S = Split Sale Paid Amount
-         * D = DCC Operation Status
-        */
-
         private void ParseData() {
 
-            _authCode = (string)GetValueOfRespField(_C, typeof(string));
-            _cashbackAmount = (decimal?)GetValueOfRespField(_Z, typeof(decimal?));
-            _gratuityAmount = (decimal?)GetValueOfRespField(_Y, typeof(decimal?));
-            _finalAmount = (decimal?)GetValueOfRespField(_M, typeof(decimal?));
-            _availableAmount = (decimal?)GetValueOfRespField(_A, typeof(decimal?));
-            _dccCode = (string)GetValueOfRespField(_U, typeof(string));
-            _dccAmount = (decimal?)GetValueOfRespField(_O, typeof(decimal?));
-            _txnSubType = (TransactionSubTypes?)GetValueOfRespField(_T, typeof(TransactionSubTypes?));
-            _dccStatus = (DynamicCurrencyStatus?)GetValueOfRespField(_D, typeof(DynamicCurrencyStatus?));
-            _splitSaleAmount = (decimal?)GetValueOfRespField(_S, typeof(decimal?));
-            _paymentMethod = (PaymentMethod?)GetValueOfRespField(_P, typeof(PaymentMethod?));
-        }
+            var tlv = new TypeLengthValue(_buffer);
 
-        private object GetValueOfRespField(byte toGet, Type returnType) {
-            var index = Array.FindIndex(_buffer, e => e == toGet);
-            if (index >= 0) {
-                // Get the length based on Documention (TLV).
-                byte[] lengthBuffer = { _buffer[index + 1], _buffer[index + 2] };
-                var length = Convert.ToInt32(Encoding.UTF8.GetString(lengthBuffer, 0, lengthBuffer.Length), 16);
+            Type stringType = typeof(string);
+            Type decimalType = typeof(decimal?);
 
-                var _arrValue = _buffer.SubArray(index + 3, length); ;
-                var endLength = index + length + 3;
-                _buffer = _buffer.SubArray(0, index).Concat(_buffer.SubArray(endLength, _buffer.Length - endLength)).ToArray();
-                var strValue = Encoding.ASCII.GetString(_arrValue, 0, _arrValue.Length);
-
-
-                if (returnType == typeof(decimal?)) {
-                    return decimal.Parse(strValue);
-                }
-                else if (returnType == typeof(string)) {
-                    return strValue;
-                }
-                else if (returnType == typeof(TransactionSubTypes?)) {
-                    return  (TransactionSubTypes)int.Parse(Convert.ToInt64(_arrValue[0]).ToString("X2"), System.Globalization.NumberStyles.HexNumber);
-                }
-                else if (returnType == typeof(DynamicCurrencyStatus?)) {
-                    return (DynamicCurrencyStatus)int.Parse(strValue);
-                }
-                else if (returnType == typeof(PaymentMethod?)) {
-                    return (PaymentMethod)int.Parse(strValue);
-                }
-                else
-                    throw new Exception("Data type not supported in parsing of response data.");
-            }
-            return null;
+            _authCode = (string)tlv.GetValue((byte)RepFieldCode.AuthCode, stringType);
+            _cashbackAmount = (decimal?)tlv.GetValue((byte)RepFieldCode.CashbackAmount, decimalType);
+            _gratuityAmount = (decimal?)tlv.GetValue((byte)RepFieldCode.GratuityAmount, decimalType);
+            _finalAmount = (decimal?)tlv.GetValue((byte)RepFieldCode.FinalTransactionAmount, decimalType);
+            _availableAmount = (decimal?)tlv.GetValue((byte)RepFieldCode.AvailableAmount, decimalType);
+            _dccCode = (string)tlv.GetValue((byte)RepFieldCode.DccCurrency, stringType);
+            _dccAmount = (decimal?)tlv.GetValue((byte)RepFieldCode.DccConvertedAmount, decimalType);
+            _txnSubType = (TransactionSubTypes?)tlv.GetValue((byte)RepFieldCode.TransactionSubType, typeof(TransactionSubTypes?));
+            _dccStatus = (DynamicCurrencyStatus?)tlv.GetValue((byte)RepFieldCode.DccOperationStatus, typeof(DynamicCurrencyStatus?));
+            _splitSaleAmount = (decimal?)tlv.GetValue((byte)RepFieldCode.SplitSalePaidAmount, decimalType);
+            _paymentMethod = (PaymentMethod?)tlv.GetValue((byte)RepFieldCode.PaymentMethod, typeof(PaymentMethod?));
         }
     }
 }
