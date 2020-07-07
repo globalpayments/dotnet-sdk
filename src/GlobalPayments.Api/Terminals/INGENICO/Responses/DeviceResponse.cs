f53842e804268ac3aeae92c3503d291771afac0a
Peter Yanong <peter.yanong@serino.com>
2020-07-07 21:42:55 +0800

    Update Ingenico DeviceResponse
    Move parsing of data in IngenicoBaseResponse.
    Move properties from IngenicoTerminalResponse to IngenicoBaseResponse

--- src/GlobalPayments.Api/Terminals/INGENICO/Responses/DeviceResponse.cs
+++ src/GlobalPayments.Api/Terminals/INGENICO/Responses/DeviceResponse.cs
@@ -8,54 +8,63 @@ using GlobalPayments.Api.Utils;
 using System;
 
 namespace GlobalPayments.Api.Terminals.Ingenico {
-    public abstract class IngenicoBaseResponse : IDeviceResponse {
-        public string Status { get; set; }
-        public string Command { get; set; }
-        public string Version { get; set; }
-        public string DeviceResponseCode { get; set; }
-        public string DeviceResponseText { get; set; }
-        public string ReferenceNumber { get; set; }
+    public abstract class IngenicoBaseResponse : DeviceResponse {
+        protected byte[] _buffer;
+        protected ParseFormat _parseFormat;
+        internal DataResponse _respField;
+
+        #region Added Properties Specific for Ingenico
+        public string DccCurrency { get; set; }
+        public DynamicCurrencyStatus? DccStatus { get; set; }
+        public decimal? DccAmount { get; set; }
+        public TransactionSubTypes? TransactionSubType { get; set; }
+        public decimal? SplitSaleAmount { get; set; }
+        public PaymentMode PaymentMode { get; set; }
+        public string CurrencyCode { get; set; }
+        public string PrivateData { get; set; }
+        public decimal? FinalTransactionAmount { get; set; }
+
+        internal string Amount { get; set; }
+
+        #endregion
+
+        internal IngenicoBaseResponse(byte[] buffer, ParseFormat format = ParseFormat.Transaction) {
+            _buffer = buffer;
+            _parseFormat = format;
+            ParseResponse(_buffer);
+        }
+
+
+        public virtual void ParseResponse(byte[] response) {
+            if (response != null) {
+
+                ReferenceNumber = Encoding.UTF8.GetString(response.SubArray(0, 2));
+                Status = ((TransactionStatus)Encoding.UTF8.GetString(response.SubArray(2, 1)).ToInt32()).ToString();
+                Amount = Encoding.UTF8.GetString(response.SubArray(3, 8));
+                PaymentMode = (PaymentMode)Encoding.UTF8.GetString(response.SubArray(11, 1)).ToInt32();
+                DccCurrency = Encoding.UTF8.GetString(response.SubArray(67, 3));
+                PrivateData = Encoding.UTF8.GetString(response.SubArray(70, response.Length - 70));
+
+                // This is for parsing of Response field for Transaction request
+                if (_parseFormat == ParseFormat.Transaction) {
+                    _respField = new DataResponse(response.SubArray(12, 55));
+                }
+            }
+        }
+
+        public override string ToString() {
+            DeviceResponseText = Encoding.UTF8.GetString(_buffer, 0, _buffer.Length);
+            return DeviceResponseText;
+        }
+
     }
 
     public class IngenicoTerminalResponse : IngenicoBaseResponse, ITerminalResponse {
 
-        internal string _transactionStatus;
-        internal decimal _amount;
-        internal PaymentMode _paymentMode;
-        internal string _privateData;
-        internal string terminalRawData;
-        internal string _currencyCode;
-        internal DataResponse _respField;
-        
-        private byte[] _buffer;
-        private ParseFormat _parseFormat;
-
-        internal IngenicoTerminalResponse(byte[] buffer, ParseFormat format = ParseFormat.Transaction) {
-            _buffer = buffer;
-            _parseFormat = format;
-            ParseResponse(buffer);
-        }
-
-        public override string ToString() {
-            base.DeviceResponseText = Encoding.UTF8.GetString(_buffer, 0, _buffer.Length);
-            return base.DeviceResponseText;
-        }
-
-        #region Added Properties Specific for Ingenico
-        public string DccCurrency { get { return _respField.DccCode; } set { } }
-        public DynamicCurrencyStatus? DccStatus { get { return _respField.DccStatus; } set { } }
-        public decimal? DccAmount { get { return _respField.DccAmount; } set { } }
-        public TransactionSubTypes? TransactionSubType { get { return _respField.TransactionSubType; } set { } }
-        public decimal? SplitSaleAmount { get { return 0; } set { } }
-        public PaymentMode PaymentMode { get { return _paymentMode; } set { } }
-        public string CurrencyCode { get { return _currencyCode; } set { } }
-        public string PrivateData { get { return _privateData; } }
-        public decimal? FinalTransactionAmount { get { return _respField.FinalAmount; } }
-        #endregion
+        internal IngenicoTerminalResponse(byte[] buffer, ParseFormat format = ParseFormat.Transaction) : base(buffer, format) { }
 
         #region Properties
-        public string ResponseText { get { return terminalRawData; } set { } }
-        public decimal? TransactionAmount { get { return _amount.ToString().ToAmount(); } set { } }
+        public decimal? TransactionAmount { get { return Amount.ToAmount(); } set { } }
         public decimal? BalanceAmount { get { return _respField.AvailableAmount; } set { } }
         public string AuthorizationCode { get { return _respField.AuthorizationCode ?? ""; } set { } }
         public decimal? TipAmount { get { return _respField.GratuityAmount; } set { } }
@@ -92,42 +101,14 @@ namespace GlobalPayments.Api.Terminals.Ingenico {
         public string ApplicationLabel { get; set; }
         public string ApplicationId { get; set; }
         public decimal? MerchantFee { get; set; }
+        public string ResponseText { get; set; }
         #endregion
 
-        /** Index
-        0 - 1 = Epos Number
-        2 - Transaction Status
-        3 - 10 = Amount
-        11 = Payment Mode
-        12 - 20 = AuthCode or starts with (C)
-        21 - 34 = Final Transaction Amount or starts with (M)
-        35 - 38 = Payment Method or starts with (P)
-        67 - 69 = Currency
-        70 - 79 = Private Data
-        */
-        public virtual void ParseResponse(byte[] response) {
-            if (response != null) {
-
-                ReferenceNumber = Encoding.UTF8.GetString(response.SubArray(0, 2));
-                _transactionStatus = ((TransactionStatus)Encoding.UTF8.GetString(response.SubArray(2, 1)).ToInt32()).ToString();
-                decimal.TryParse(Encoding.UTF8.GetString(response.SubArray(3, 8)), out _amount);
-                _paymentMode = (PaymentMode)Encoding.UTF8.GetString(response.SubArray(11, 1)).ToInt32();
-                _currencyCode = Encoding.UTF8.GetString(response.SubArray(67, 3));
-                _privateData = Encoding.UTF8.GetString(response.SubArray(70, response.Length - 70));
-                Status = _transactionStatus;
-
-                // This is for parsing of Response field for Transaction request
-                if (_parseFormat == ParseFormat.Transaction) {
-                    _respField = new DataResponse(response.SubArray(12, 55));
-                }
-            }
-        }
     }
 
     public class IngenicoTerminalReportResponse : IngenicoBaseResponse, ITerminalReport {
-        private byte[] _buffer;
 
-        internal IngenicoTerminalReportResponse(byte[] buffer) {
+        internal IngenicoTerminalReportResponse(byte[] buffer) : base(buffer) {
             _buffer = buffer;
             Status = _buffer.Length > 0 ? "SUCCESS" : "FAILED";
         }
@@ -135,7 +116,5 @@ namespace GlobalPayments.Api.Terminals.Ingenico {
         public override string ToString() {
             return Encoding.ASCII.GetString(_buffer);
         }
-
-
     }
 }
