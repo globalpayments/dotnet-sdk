3851ed4ddf1b2de22974933d9c6525ac21307c96
Peter Yanong <peter.yanong@serino.com>
2020-07-07 21:58:32 +0800

    Update ReportResponse remove unnecessary ovveride method

--- src/GlobalPayments.Api/Terminals/INGENICO/Responses/ReportResponse.cs
+++ src/GlobalPayments.Api/Terminals/INGENICO/Responses/ReportResponse.cs
@@ -7,10 +7,5 @@ namespace GlobalPayments.Api.Terminals.Ingenico.Responses {
         internal ReportResponse(byte[] buffer) : base(buffer) {
             ParseResponse(buffer);
         }
-
-        public override void ParseResponse(byte[] response) {
-            base.ParseResponse(response);
-            _privateData = Encoding.UTF8.GetString(response.SubArray(70, response.Length - 70));
-        }
     }
 }
