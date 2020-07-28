eea6f9f752754ea5d4a31c47a0a4e6678faea5fd
BuildMaster <EntApp_DevPortal@e-hps.com>
2020-07-28 08:43:14 -0400

    BuildMaster release: v1.7.4

--- tests/GlobalPayments.Api.Tests/Terminals/Pax/PaxReportTests.cs
+++ tests/GlobalPayments.Api.Tests/Terminals/Pax/PaxReportTests.cs
@@ -19,7 +19,8 @@ namespace GlobalPayments.Api.Tests.Terminals.Pax {
             _device = DeviceService.Create(new ConnectionConfig {
                 DeviceType = DeviceType.PAX_S300,
                 ConnectionMode = ConnectionModes.TCP_IP,
-                IpAddress = "192.168.0.31",
+                IpAddress = "10.12.220.172",
+                //IpAddress = "192.168.0.31",
                 Port = "10009",
                 Timeout = 30000,
                 RequestIdProvider = new RandomIdProvider()
